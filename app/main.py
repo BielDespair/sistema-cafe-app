c6ca038c1092eccd3c2f41e028032571051696b8
BielDespair <gabrielvista7@gmail.com>
2026-07-14 17:22:06 -0300

    CORS Removed

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ allowed_origins = os.getenv(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins= ["*"], #allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
